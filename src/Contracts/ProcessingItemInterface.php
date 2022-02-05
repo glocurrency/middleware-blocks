e2c27b5e0cac685659740839203b29f22b2c6bc5
Ivan Stasiuk <brokeyourbike@gmail.com>
2022-02-05 16:24:54 +0200

    fix: use proper enum, and add update method

--- src/Contracts/ProcessingItemInterface.php
+++ src/Contracts/ProcessingItemInterface.php
@@ -2,13 +2,14 @@
 
 namespace GloCurrency\MiddlewareBlocks\Contracts;
 
-use GloCurrency\MiddlewareBlocks\Enums\RecipientStateCodeEnum;
+use GloCurrency\MiddlewareBlocks\Enums\ProcessingItemStateCodeEnum;
 use GloCurrency\MiddlewareBlocks\Contracts\TransactionInterface;
 
 interface ProcessingItemInterface
 {
     public function getId(): string;
-    public function getStateCode(): RecipientStateCodeEnum;
+    public function getStateCode(): ProcessingItemStateCodeEnum;
     public function getStateCodeReason(): ?string;
+    public function updateStateCode(ProcessingItemStateCodeEnum $stateCode, ?string $stateCodeReason): bool;
     public function getTransaction(): ?TransactionInterface;
 }
