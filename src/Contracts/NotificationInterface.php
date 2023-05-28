bbbd6b52b6e49c2317175e6c3eac6005c90e52a9
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-05-28 17:44:58 +0100

    fix!: make for country code

--- src/Contracts/NotificationInterface.php
+++ src/Contracts/NotificationInterface.php
@@ -4,7 +4,7 @@ namespace GloCurrency\MiddlewareBlocks\Contracts;
 
 interface NotificationInterface
 {
-    public static function makeFor(string $phoneNumber, string $message): self;
+    public static function makeFor(string $countryCode, string $phoneNumber, string $message): self;
     public function wasSent(): bool;
     public function getText(): string;
 }
